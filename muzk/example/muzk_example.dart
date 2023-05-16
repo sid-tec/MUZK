56f1a14a868b2eaf00729a8a33676a57ce04fcbf
sid-tec <sid.tec.rio@gmail.com>
2023-05-16 15:09:20 -0300

    Criado

---
+++ muzk/example/muzk_example.dart
@@ -0,0 +1,6 @@
+import 'package:muzk/muzk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ muzk/lib/muzk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library muzk;
+
+export 'src/muzk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
